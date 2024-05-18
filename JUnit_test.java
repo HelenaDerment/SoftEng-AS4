ceb6b8a0065704487ee8535b82f3e6498b961803
Helena Derment <s4006303@rmit.student.edu.au>
2024-05-18 10:42:19 +1000

    Test case 2

--- JUnit_test.java
+++ JUnit_test.java
@@ -17,7 +17,7 @@ public void testAddPostValid() {
 //Test Case #1 - Data 2
 public void testAddPostValid() {
     String[] tags = {"tag1", "tag2", "tag3"};
-    Post post = new Post(3, "How to create a user in linux using python", 
+    Post post = new Post(2, "How to create a user in linux using python", 
     "How do I create a user in Linux using Python? I mean, I know about the subprocess module and thought about calling 'adduser' and 
     passing all the parameters at once, but the 'adduser' command asks some questions like password, full name, phone and stuff. 
     How would I answer this questions using subprocess?.", tags, "Difficult", "Highly Needed");
@@ -25,8 +25,25 @@ public void testAddPostValid() {
 }
 
 //Test Case #2 - Data 1
+public void testAddPostInvalidTitle() {
+    String[] tags = {"tag1", "tag2"};
+    Post post = new Post(3, "A 22 letter String", 
+    "The result after decoding is then 16 bytes (every 3 bytes becomes 4 characters, 
+    so we get 20 from the first 15 bytes and 2 characters more to encode the last byte; 
+    you'll note that the final character has fewer options than the other ones, because it really encodes 2 trailing bits 
+    (so we'd expect 4 different ones, including A for 00 etc.). Read up on base64 encoding if you want the nitty gritty", tags, "Easy", "Ordinary");
+    assertTrue(post.addPost());
+}
 
 //Test Case #2 - Data 2
+public void testAddPostInvalidTitle() {
+    String[] tags = {"tag1", "tag2"};
+    Post post = new Post(4, "10 Python Tips",
+    "Consistency is very important when you are learning a new language. We recommend making a commitment to code every day. 
+    It may be hard to believe, but muscle memory plays a large part in programming. Committing to coding everyday will really help develop that muscle memory. 
+    Though it may seem daunting at first, consider starting small with 25 minutes everyday and working your way up from there.", tags, "Easy", "Ordinary");
+    assertTrue(post.addPost());
+}
 
 //Test Case #3 - Data 1
 
