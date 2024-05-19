007e736692517795ccae38a6c4196a86dba37c3d
Helena Derment <s4006303@rmit.student.edu.au>
2024-05-19 17:29:22 +1000

    Test Case 3 & 4

--- JUnit_test.java
+++ JUnit_test.java
@@ -32,7 +32,7 @@ public void testAddPostInvalidTitle() {
     so we get 20 from the first 15 bytes and 2 characters more to encode the last byte; 
     you'll note that the final character has fewer options than the other ones, because it really encodes 2 trailing bits 
     (so we'd expect 4 different ones, including A for 00 etc.). Read up on base64 encoding if you want the nitty gritty", tags, "Easy", "Ordinary");
-    assertTrue(post.addPost());
+    assertFalse(post.addPost());
 }
 
 //Test Case #2 - Data 2
@@ -42,16 +42,51 @@ public void testAddPostInvalidTitle() {
     "Consistency is very important when you are learning a new language. We recommend making a commitment to code every day. 
     It may be hard to believe, but muscle memory plays a large part in programming. Committing to coding everyday will really help develop that muscle memory. 
     Though it may seem daunting at first, consider starting small with 25 minutes everyday and working your way up from there.", tags, "Easy", "Ordinary");
-    assertTrue(post.addPost());
+    assertFalse(post.addPost());
 }
 
 //Test Case #3 - Data 1
+public void testAddPostValidBody() {
+    String[] tags = {"tag1", "tag2"};
+    Post post = new Post(5, "Software Engineering Fundamentals for Beginners",
+    "Online learning platforms provide a flexible and affordable way to build software engineering skills without needing to enroll in a formal degree program. 
+    With the wealth of free and paid online courses, certifications, books, and other resources now available, self-driven learners can craft personalized 
+    curriculums tailored to their goals.", tags, "Easy", "Ordinary");
+    assertTrue(post.addPost());
+}
 
 //Test Case #3 - Data 2
+public void testAddPostValidBody() {
+    String[] tags = {"tag1", "tag2", "tag3"};
+    Post post = new Post(6, "Decrypting an asymmetric public key",
+    "This topic provides information about creating and using a key for asymmetric encryption using an RSA key. Asymmetric encryption uses the public key portion of the 
+    asymmetric key and decryption uses the private key portion of the key. Cloud Key Management Service provides functionality to retrieve the public key and 
+    functionality to decrypt ciphertext that was encrypted with the public key. Cloud KMS does not allow direct access to the private key.", tags, "Difficult", "Immediately Needed");
+    assertTrue(post.addPost());
+    
+}
 
 //Test Case #4 - Data 1
+public void testAddPostInvalidTag() {
+    String[] tags = {"Software"};
+    Post post = new Post(7, "Software Testing Explanation", 
+    "Software testing is the process of checking the quality, functionality, and performance of a software product before launching. 
+    To do software testing, testers either interact with the software manually or execute test scripts to find bugs and errors, ensuring that the 
+    software works as expected. Software testing is also done to see if business logic is fulfilled, or if there are any missing gaps in requirements 
+    that need immediate tackles." ,tags, "Easy", "Ordinary");
+    assertFalse(post.addPost());
+}
 
 //Test Case #4 - Data 2
+public void testAddPostInvalidTag() {
+    String[] tags = {"tag1", "tooLongTagHere123", "tag3"};
+    Post post = new Post(8, "AI Innovations in Healthcare",
+    "The fusion of physiological data with lifestyle and environmental factors is paving the way for deep phenotyping. Coupled with genomics, this is set to take 
+    healthcare to new heights. Bloomer Tech is combining cutting-edge fabric technology with ML to transform clothes, for example, women's bras, into wearable medical 
+    devices. AI's role in generating digital biomarkers promises transformative impacts, especially for diseases that disproportionately affect women. Bloomer Tech’s 
+    focus lies in the cardiovascular system in women, given the challenges in diagnostics and treatment." tags, "Difficult", "Highly Needed");
+    assertFalse(post.addPost());
+}
 
 //Test Case #5 - Data 1
 
